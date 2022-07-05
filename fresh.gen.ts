5535586c21a7cbb4d468e041efdcd0bd9354f173
Pheon-Dev <devpheon@gmail.com>
2022-07-05 09:06:09 +0300

    ADD: Static Routing

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,16 +2,18 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $0 from "./routes/Dashboard.tsx";
+import * as $1 from "./routes/[name].tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/Dashboard.tsx": $0,
+    "./routes/[name].tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
--- routes/Dashboard.tsx
+++ routes/Dashboard.tsx
@@ -1,9 +1,8 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img
@@ -14,7 +13,7 @@ export default function Home() {
       <p class={tw`my-6`}>
         Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
       </p>
-      <Counter start={3} />
     </div>
   );
 }
+
