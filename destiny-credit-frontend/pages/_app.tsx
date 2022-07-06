2f649863267dfab2c4c2510ecf20192343b2a6e7
Pheon-Dev <devpheon@gmail.com>
2022-07-06 09:53:06 +0300

    SSR: Server Side Rendering State Management

--- destiny-credit-frontend/pages/_app.tsx
+++ destiny-credit-frontend/pages/_app.tsx
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
