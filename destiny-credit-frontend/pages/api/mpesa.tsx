f4c0301a3724caca48ccd885ca1f71563692cfb0
Pheon-Dev <devpheon@gmail.com>
2022-07-13 12:10:29 +0300

    ADD: [POST] Request Service Handler

--- destiny-credit-frontend/pages/api/mpesa.tsx
+++ destiny-credit-frontend/pages/api/mpesa.tsx
@@ -100,6 +100,89 @@ export class HttpService{
       } catch (error) {
         reject(error);
       }
+    });
+  }
+
+  post<T = unknown, K extends any = any>(
+    path: string,
+    payload: K,
+    { headers }: HttpServiceConfig,
+  ): Promise<HttpServiceResponse<T>> {
+    return new Promise<HttpServiceResponse<T>>((resolve, reject) => {
+      try {
+        const request = this.uri.protocol === 'https:' ? httpsRequest : httpRequest;
+
+        const data = JSON.stringify(payload);
+
+        const clientRequest = request(
+          {
+            protocol: this.uri.protocol,
+            hostname: this.uri.hostname,
+            path,
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Content-Length': data.length,
+              ...this.headers,
+              ...headers,
+            },
+          },
+          (response) => {
+            const { headers, statusCode, statusMessage } = response;
+
+            let dataChunks = '';
+
+            response.on('data', (chunk) => {
+              dataChunks += chunk;
+            });
+
+            response.on('data', () => {
+              let data: any;
+
+              try {
+                data = JSON.parse(dataChunks);
+              } catch (error) {
+                data = dataChunks?.toString();
+              }
+
+              const result = {
+                protocol: this.uri.protocol,
+                hostname: this.uri.hostname,
+                path: path,
+                method: 'POST',
+                headers,
+                statusCode,
+                statusMessage,
+                data,
+              };
+
+              if (Number(statusCode) >= 200 && Number(statusCode) < 300) {
+                return resolve({
+                  protocol: `${this.uri.protocol}`,
+                  hostname: `${this.uri.hostname}`,
+                  path: path,
+                  method: 'POST',
+                  headers,
+                  statusCode: Number(statusCode),
+                  statusMessage: `${statusMessage}`,
+                  data,
+                });
+              };
+
+              reject(result);
+            });
+          },
+        );
+
+        clientRequest.on('error', (error) => {
+          reject(error);
+        });
+
+        clientRequest.write(data);
+        clientRequest.end();
+      } catch (error) {
+        reject(error);
+      }
     })
   }
 }
