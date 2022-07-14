10d005d835816ad83cead65fb81ebd0fce17fbf8
Pheon-Dev <devpheon@gmail.com>
2022-07-14 15:49:16 +0300

    EDT: M-PESA Function Handler

--- destiny-credit-frontend/pages/api/m-pesa.tsx
+++ destiny-credit-frontend/pages/api/m-pesa.tsx
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Mpesa } from "mpesa-api";
 import axios from "axios";
 import {
   CredentialsInterface,
@@ -27,6 +26,7 @@ const CONSUMER_KEY = process.env.NEXT_PUBLIC_CONSUMER_KEY;
 const CONSUMER_SECRET = process.env.NEXT_PUBLIC_CONSUMER_SECRET;
 const INITIATOR_PASSWORD = process.env.NEXT_PUBLIC_INITIATOR_PASSWORD;
 const SECURITY_CREDENTIAL = process.env.NEXT_PUBLIC_SECURITY_CREDENTIAL;
+const PASS_KEY = process.env.NEXT_PUBLIC_SECURITY_CREDENTIAL;
 const PAY_BILL = process.env.NEXT_PUBLIC_PAY_BILL;
 const TILL_NUMBER = process.env.NEXT_PUBLIC_TILL_NUMBER;
 const PHONE_NUMBER = process.env.NEXT_PUBLIC_PHONE_NUMBER;
@@ -422,29 +422,28 @@ export default function handler(
 
   const environment = "sandbox";
 
-  const mpesa = new Mpesa(credentials, environment);
-  // const mpesa = new MpesaApi(credentials, environment);
+  const mpesa = new MpesaApi(credentials, environment);
 
-  // mpesa
-  //   .lipaNaMpesaQuery({
-  //     // BusinessShortCode: 123456,
-  //     BusinessShortCode: Number(PAY_BILL_secret),
-  //     CheckoutRequestID: "Checkout Request ID",
-  //     passKey: `${CONSUMER_KEY}`,
-  //   })
-  //   .then((response) => {
-  //     console.log(response);
-  //     res.status(200).json({ name: `${JSON.stringify(response, null, 2)}` });
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //     res.status(400).json({ name: `${JSON.stringify(error, null, 2)}` });
-  //   });
+  mpesa
+    .lipaNaMpesaQuery({
+      // BusinessShortCode: 123456,
+      BusinessShortCode: Number(PAY_BILL),
+      CheckoutRequestID: "Checkout Request ID",
+      passKey: `${CONSUMER_KEY}`,
+    })
+    .then((response) => {
+      console.log(response);
+      res.status(200).json({ name: `${JSON.stringify(response, null, 2)}` });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json({ name: `${JSON.stringify(error, null, 2)}` });
+    });
 
   // mpesa
   //   .lipaNaMpesaOnline({
   //     BusinessShortCode: Number(PAY_BILL),
-  //     passKey: "Lipa Na M-PESA Pass Key",
+  //     passKey: `${PASS_KEY}`,
   //     TransactionDesc: "Transaction Desc",
   //     TransactionType: "CustomerPayBillOnline",
   //     PartyA: `${PHONE_NUMBER}`,
@@ -465,22 +464,22 @@ export default function handler(
   //     res.status(400).json({ name: `${JSON.stringify(error, undefined, 2)}` });
   //   });
 
-  mpesa
-    .c2bSimulate({
-      ShortCode: Number(PAY_BILL),
-      Amount: 1000,
-      Msisdn: Number(PHONE_NUMBER),
-      CommandID: "CustomerPayBillOnline",
-      BillRefNumber: "8986987",
-    })
-    .then((response) => {
-      console.log(response);
-      res
-        .status(200)
-        .json({ name: `${JSON.stringify(response, undefined, 2)}` });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json({ name: `${JSON.stringify(error, undefined, 2)}` });
-    });
+  // mpesa
+  //   .c2bSimulate({
+  //     ShortCode: Number(PAY_BILL),
+  //     Amount: 1000,
+  //     Msisdn: Number(PHONE_NUMBER),
+  //     CommandID: "CustomerPayBillOnline",
+  //     BillRefNumber: "8986987",
+  //   })
+  //   .then((response) => {
+  //     console.log(response);
+  //     res
+  //       .status(200)
+  //       .json({ name: `${JSON.stringify(response, undefined, 2)}` });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     res.status(400).json({ name: `${JSON.stringify(error, undefined, 2)}` });
+  //   });
 }
