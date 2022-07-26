5f9968fe10f3e691c2bd4183927de5f7af1f72c3
Pheon-Dev <devpheon@gmail.com>
2022-07-26 12:54:13 +0300

    ADD: Request Body Logger

--- destiny-credit-frontend/pages/api/confirmation.ts
+++ destiny-credit-frontend/pages/api/confirmation.ts
@@ -72,6 +72,7 @@ export default async function handler(
       // res.status(200).json(confirmationReq);
       //
       // console.log(JSON.stringify(confirmationReq));
+      console.log(req.body);
       res.status(200).json({
         ResultCode: 0,
         ResultDesc: "Accepted",
