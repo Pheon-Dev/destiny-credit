702bee118fc73e92730f0edf30638ddf8dd0a5d1
Pheon-Dev <devpheon@gmail.com>
2022-08-12 10:42:15 +0300

    ADD: Write to Database On Response Match

--- pages/api/confirmation.ts
+++ pages/api/confirmation.ts
@@ -14,27 +14,58 @@ async function confirm(req: NextApiRequest, res: NextApiResponse) {
       form.parse(req, function (err: any, fields: any, files: any) {
         if (err) return reject(err);
         resolve({ fields, files });
+        async function writeData() {
+          if (fields?.TransactionType === "Pay Bill") {
+            const url = "https://destiny-credit.vercel.app/api/transaction";
+            const headers = {
+              "Content-Type": "application/json",
+            };
+
+            const body = {
+              transactionType: fields?.TransactionType,
+              transID: fields?.TransID,
+              transTime: fields?.TransTime,
+              transAmount: fields?.TransAmount,
+              businessShortCode: fields?.BusinessShortCode,
+              billRefNumber: fields?.BillRefNumber,
+              invoiceNumber: fields?.InvoiceNumber,
+              orgAccountBalance: fields?.OrgAccountBalance,
+              thirdPartyTransID: fields?.ThirdPartyTransID,
+              msisdn: fields?.Msisdn,
+              firstName: fields?.FirstName,
+              middleName: fields?.MiddleName,
+              lastName: fields?.LastName,
+            };
+            const results = await axios.request({
+              data: JSON.stringify(body),
+              method: "POST",
+              url: url,
+              headers: headers,
+            });
+            console.log(results);
+          }
+          if (fields?.ResultDesc === "Accepted") {
+            const url = "https://destiny-credit.vercel.app/api/test";
+            const headers = {
+              "Content-Type": "application/json",
+            };
+
+            const body = {
+              transactionTest: fields?.ResultDesc,
+            };
+            const results = await axios.request({
+              data: JSON.stringify(body),
+              method: "POST",
+              url: url,
+              headers: headers,
+            });
+            console.log(results);
+          }
+        }
+        writeData();
       });
     });
 
-    const url = "https://destiny-credit.vercel.app/api/test";
-    const headers = {
-      "Content-Type": "application/json",
-    };
-    console.log("--------")
-    console.log(data)
-    console.log("--------")
-    const body_data = {
-      transactionTest: data,
-    };
-    const results = await axios.request({
-      data: JSON.stringify(body_data),
-      method: "POST",
-      url: url,
-      headers: headers,
-    });
-
-    console.log(results);
     const body = JSON.stringify(data);
     console.log(body);
   } catch (error) {
