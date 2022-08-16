5316d280ad23a9a38b47d6f983fe38cad5a3ae4a
Pheon-Dev <devpheon@gmail.com>
2022-08-16 11:26:18 +0300

    ADD: Revalidation Endpoint [api/revalidate]

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const REVALIDATE_SECRET = process.env.NEXT_PUBLIC_REVALIDATE_SECRET;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.query.secret !== REVALIDATE_SECRET) {
+    return res.status(401).json({message: 'Invalid Token'});
+  }
+  async function revalidation() {
+    try {
+      await res.unstable_revalidate('/');
+      return res.json({ revalidated: true });
+    } catch (error) {
+      return res.status(500).send('Error Revlidating');
+    }
+  }
+  revalidation();
+}
+
