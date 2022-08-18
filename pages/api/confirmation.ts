a48dd7dd7df45284614edd6182f0c792f45a0a1a
Pheon-Dev <devpheon@gmail.com>
2022-08-18 08:10:43 +0300

    ADD: Write to Redis DB

--- pages/api/confirmation.ts
+++ pages/api/confirmation.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 import formidable from "formidable";
 import { createClient } from "@supabase/supabase-js";
 
@@ -21,30 +22,51 @@ type Fields = {
   lastName: string;
 };
 
-export default async function confirm(req: NextApiRequest, res: NextApiResponse) {
-    async function write({data}: {data: Fields[]}) {
-        const supabaseAdmin = createClient(
-          SUPABASE_URL || "",
-          SUPABASE_SERVICE_ROLE_KEY || ""
-        );
-        await supabaseAdmin.from("transactions").insert([
-          {
-            transactionType: data[0].transactionType,
-            transID: data[0].transID,
-            transTime: data[0].transTime,
-            transAmount: data[0].transAmount,
-            businessShortCode: data[0].businessShortCode,
-            billRefNumber: data[0].billRefNumber,
-            invoiceNumber: data[0].invoiceNumber,
-            orgAccountBalance: data[0].orgAccountBalance,
-            thirdPartyTransID: data[0].thirdPartyTransID,
-            msisdn: data[0].msisdn,
-            firstName: data[0].firstName,
-            middleName: data[0].middleName,
-            lastName: data[0].lastName,
-          },
-        ]);
-    }
+export default async function confirm(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  async function write({ data }: { data: Fields[] }) {
+    const url_db = "https://destiny-credit.vercel.app/api/transaction";
+    const headers_db = {
+      "Content-Type": "application/json",
+    };
+
+    const body = {
+      transactionType: data[0].transactionType,
+      transID: data[0].transID,
+      transTime: data[0].transTime,
+      transAmount: data[0].transAmount,
+      businessShortCode: data[0].businessShortCode,
+      billRefNumber: data[0].billRefNumber,
+      invoiceNumber: data[0].invoiceNumber,
+      orgAccountBalance: data[0].orgAccountBalance,
+      thirdPartyTransID: data[0].thirdPartyTransID,
+      msisdn: data[0].msisdn,
+      firstName: data[0].firstName,
+      middleName: data[0].middleName,
+      lastName: data[0].lastName,
+    };
+
+    const res_db = await axios.request({
+      data: JSON.stringify(body),
+      method: "POST",
+      url: url_db,
+      headers: headers_db,
+    });
+
+    console.log(res_db.data);
+
+    const supabaseAdmin = createClient(
+      SUPABASE_URL || "",
+      SUPABASE_SERVICE_ROLE_KEY || ""
+    );
+    await supabaseAdmin.from("transactions").insert([
+      {
+        body,
+      },
+    ]);
+  }
 
   try {
     res.status(200).json({
@@ -69,12 +91,12 @@ export default async function confirm(req: NextApiRequest, res: NextApiResponse)
           invoiceNumber: fields.InvoiceNumber,
           orgAccountBalance: fields.OrgAccountBalance,
           thirdPartyTransID: fields.ThirdPartyTransID,
-          msisdn: fields.Msisdn,
+          msisdn: fields.MSISDN,
           firstName: fields.FirstName,
           middleName: fields.MiddleName,
           lastName: fields.LastName,
         });
-        write({data: result});
+        write({ data: result });
       });
     });
     const body = JSON.stringify(data);
