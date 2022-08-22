086c8a9f7efce9b149cdeab863de6fa37264c5ef
Pheon-Dev <devpheon@gmail.com>
2022-08-22 14:54:55 +0300

    EDT: [api/confirmation] API Endpoint

--- pages/api/confirmation.ts
+++ pages/api/confirmation.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import formidable from "formidable";
+import { Fields } from "../../types";
 
 export default async function confirm(
   req: NextApiRequest,
@@ -10,17 +11,34 @@ export default async function confirm(
       ResultCode: 0,
       ResultDesc: "Accepted",
     });
+    let result: Array<Fields> = [];
 
     const data = await new Promise(function (resolve, reject) {
       const form = new formidable.IncomingForm({ keepExtensions: true });
       form.parse(req, function (err: any, fields: any, files: any) {
         if (err) return reject(err);
         resolve({ fields, files });
+        // res.json({ files, fields });
+        result.push({
+          transactionType: fields.TransactionType,
+          transID: fields.TransID,
+          transTime: fields.TransTime,
+          transAmount: fields.TransAmount,
+          businessShortCode: fields.BusinessShortCode,
+          billRefNumber: fields.BillRefNumber,
+          invoiceNumber: fields.InvoiceNumber,
+          orgAccountBalance: fields.OrgAccountBalance,
+          thirdPartyTransID: fields.ThirdPartyTransID,
+          msisdn: fields.MSISDN,
+          firstName: fields.FirstName,
+          middleName: fields.MiddleName,
+          lastName: fields.LastName,
+        });
+        console.log(result);
       });
     });
-    const body = JSON.stringify(data);
-
-    console.log(body);
+    // const body = JSON.stringify(data);
+    // console.log(body);
 
   } catch (error) {
     console.log(error);
