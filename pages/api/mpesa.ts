006ccac9472e52c6846d69e595fffce250a23b5f
Pheon-Dev <devpheon@gmail.com>
2022-08-23 09:01:08 +0300

    EDT: Mute Writing to Database

--- pages/api/mpesa.ts
+++ pages/api/mpesa.ts
@@ -33,6 +33,7 @@ export default async function handler(
         str_ndate.split('/')[1] +
         "-" +
         str_ndate.split('/')[0];
+
       const url = `https://logtail.com/api/v1/query?query=transactionType: &from=${new_date} 00:00:00&to=${now_date} ${str_time}&source_ids=158744`;
       const token = LOGTAIL_API_TOKEN;
       const headers = {
@@ -46,121 +47,121 @@ export default async function handler(
 
       const log = response.data;
 
-      if (log.data.length > 0) {
-        let counter: number = 0;
-        while (counter < log.data.length) {
-          const data_res: string = log.data[counter]?.message
-            .split("{")[1]
-            .split("}")[0];
-          // counter++;
-          let transactionType: string = data_res
-            .split(",")[0]
-            .split(":")[1]
-            .split("'")[1];
-          let transID: string = data_res
-            .split(",")[1]
-            .split(":")[1]
-            .split("'")[1];
-          let transTime: string = data_res
-            .split(",")[2]
-            .split(":")[1]
-            .split("'")[1];
-          let transAmount: string = data_res
-            .split(",")[3]
-            .split(":")[1]
-            .split("'")[1];
-          let businessShortCode: string = data_res
-            .split(",")[4]
-            .split(":")[1]
-            .split("'")[1];
-          let billRefNumber: string = data_res
-            .split(",")[5]
-            .split(":")[1]
-            .split("'")[1];
-          let invoiceNumber: string = data_res
-            .split(",")[6]
-            .split(":")[1]
-            .split("'")[1];
-          let orgAccountBalance: string = data_res
-            .split(",")[7]
-            .split(":")[1]
-            .split("'")[1];
-          let thirdPartyTransID: string = data_res
-            .split(",")[8]
-            .split(":")[1]
-            .split("'")[1];
-          let msisdn: string = data_res
-            .split(",")[9]
-            .split(":")[1]
-            .split("'")[1];
-          let firstName: string = data_res
-            .split(",")[10]
-            .split(":")[1]
-            .split("'")[1];
-          let middleName: string = data_res
-            .split(",")[11]
-            .split(":")[1]
-            .split("'")[1];
-          let lastName: string = data_res
-            .split(",")[12]
-            .split(":")[1]
-            .split("'")[1];
-
-          let result: Array<Fields> = [];
-
-          result.push({
-            transactionType: transactionType,
-            transAmount: transAmount,
-            businessShortCode: businessShortCode,
-            billRefNumber: billRefNumber,
-            transID: transID,
-            transTime: transTime,
-            invoiceNumber: invoiceNumber,
-            orgAccountBalance: orgAccountBalance,
-            thirdPartyTransID: thirdPartyTransID,
-            msisdn: msisdn,
-            firstName: firstName,
-            middleName: middleName,
-            lastName: lastName,
-          });
-
-          const { data, error } = await supabase
-            .from("transactions")
-            .select()
-            .textSearch("transTime", `'${transTime}'`);
-
-          if (error)
-            return res
-              .status(500)
-              .json({ message: "Error Fetching Data From Database" });
-
-          if (data.length === 0) {
-            await supabase.from("transactions").insert([
-              {
-                transactionType: transactionType,
-                transID: transID,
-                transTime: transTime,
-                transAmount: transAmount,
-                businessShortCode: businessShortCode,
-                billRefNumber: billRefNumber,
-                invoiceNumber: invoiceNumber,
-                orgAccountBalance: orgAccountBalance,
-                thirdPartyTransID: thirdPartyTransID,
-                msisdn: msisdn,
-                firstName: firstName,
-                middleName: middleName,
-                lastName: lastName,
-              },
-            ]);
-
-            counter++;
-          }
-
-          if (data.length !== 0) {
-            counter++;
-          }
-        }
-      }
+      // if (log.data.length > 0) {
+      //   let counter: number = 0;
+      //   while (counter < log.data.length) {
+      //     const data_res: string = log.data[counter]?.message
+      //       .split("{")[1]
+      //       .split("}")[0];
+      //     // counter++;
+      //     let transactionType: string = data_res
+      //       .split(",")[0]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let transID: string = data_res
+      //       .split(",")[1]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let transTime: string = data_res
+      //       .split(",")[2]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let transAmount: string = data_res
+      //       .split(",")[3]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let businessShortCode: string = data_res
+      //       .split(",")[4]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let billRefNumber: string = data_res
+      //       .split(",")[5]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let invoiceNumber: string = data_res
+      //       .split(",")[6]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let orgAccountBalance: string = data_res
+      //       .split(",")[7]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let thirdPartyTransID: string = data_res
+      //       .split(",")[8]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let msisdn: string = data_res
+      //       .split(",")[9]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let firstName: string = data_res
+      //       .split(",")[10]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let middleName: string = data_res
+      //       .split(",")[11]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //     let lastName: string = data_res
+      //       .split(",")[12]
+      //       .split(":")[1]
+      //       .split("'")[1];
+      //
+      //     let result: Array<Fields> = [];
+      //
+      //     result.push({
+      //       transactionType: transactionType,
+      //       transAmount: transAmount,
+      //       businessShortCode: businessShortCode,
+      //       billRefNumber: billRefNumber,
+      //       transID: transID,
+      //       transTime: transTime,
+      //       invoiceNumber: invoiceNumber,
+      //       orgAccountBalance: orgAccountBalance,
+      //       thirdPartyTransID: thirdPartyTransID,
+      //       msisdn: msisdn,
+      //       firstName: firstName,
+      //       middleName: middleName,
+      //       lastName: lastName,
+      //     });
+      //
+      //     const { data, error } = await supabase
+      //       .from("transactions")
+      //       .select()
+      //       .textSearch("transTime", `'${transTime}'`);
+      //
+      //     if (error)
+      //       return res
+      //         .status(500)
+      //         .json({ message: "Error Fetching Data From Database" });
+      //
+      //     if (data.length === 0) {
+      //       await supabase.from("transactions").insert([
+      //         {
+      //           transactionType: transactionType,
+      //           transID: transID,
+      //           transTime: transTime,
+      //           transAmount: transAmount,
+      //           businessShortCode: businessShortCode,
+      //           billRefNumber: billRefNumber,
+      //           invoiceNumber: invoiceNumber,
+      //           orgAccountBalance: orgAccountBalance,
+      //           thirdPartyTransID: thirdPartyTransID,
+      //           msisdn: msisdn,
+      //           firstName: firstName,
+      //           middleName: middleName,
+      //           lastName: lastName,
+      //         },
+      //       ]);
+      //
+      //       counter++;
+      //     }
+      //
+      //     if (data.length !== 0) {
+      //       counter++;
+      //     }
+      //   }
+      // }
 
       res
         .status(200)
