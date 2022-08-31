15b7f8f74c6583fe9e69b63fef10c51ddb388bd3
Pheon-Dev <devpheon@gmail.com>
2022-08-31 15:33:00 +0300

    EDT: Maintenance Page

--- pages/loans/maintenance.tsx
+++ pages/loans/maintenance.tsx
@@ -1,60 +1,13 @@
-import React, { forwardRef, useState } from "react";
-import { PrismaClient } from "@prisma/client";
-import { Members, MemberProps } from "../../types";
-import { GetServerSideProps } from "next";
-import {
-  Autocomplete,
-  Group,
-  Text,
-} from "@mantine/core";
+import React from "react";
 
-const Page = ({ data }: { data: Members[] }) => {
-  const [member, setMember] = useState('');
-
-  const members = data.map((item) => ({ ...item, value: item.firstName + " " + item.lastName }));
-
-  const AutoCompleteItem = forwardRef<HTMLDivElement, MemberProps>(
-    ({ firstName, lastName, memberId, ...others }: MemberProps, ref) => (
-      <div ref={ref} {...others}>
-        <Group noWrap>
-          <div>
-            <Text>{memberId}: {firstName} {lastName}</Text>
-          </div>
-        </Group>
-      </div>
-    )
-  );
-  /* console.log(members) */
+const Page = () => {
+  console.log("Maintenance Page")
   return (
-      <>
-      <Autocomplete
-        label="Select Customer"
-        placeholder="Select Customer"
-        itemComponent={AutoCompleteItem}
-        data={members}
-        value={member}
-        onChange={setMember}
-        filter={(value, item) =>
-          item.value.toLowerCase().includes(value.toLowerCase().trim()) ||
-          item.firstName.toLowerCase().includes(value.toLowerCase().trim())
-        }
-      />
-      </>
+    <>
+      <div>Maintenance</div>
+    </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const prisma = new PrismaClient();
-
-  let data = await prisma.members.findMany();
-
-  data = JSON.parse(JSON.stringify(data));
-
-  return {
-    props: {
-      data,
-    },
-  };
 };
 
 export default Page;
+
