f75bff785d39389e4e49b2ef0c798f847982d1cf
Pheon-Dev <devpheon@gmail.com>
2022-10-06 13:18:09 +0300

    EDT: Comment Out Logger Link tRPC

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,4 +1,4 @@
-import { httpBatchLink, httpLink, loggerLink } from "@trpc/client";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import { NextPageContext } from "next";
 import superjson from "superjson";
@@ -24,37 +24,34 @@ export const trpc = createTRPCNext<AppRouter, SSRContext>({
     return {
       transformer: superjson,
       links: [
-        loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error),
-        }),
-        httpLink({
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
-        /* httpBatchLink({ */
-        /*   url: `${getBaseUrl()}/api/trpc`, */
-        /*   headers() { */
-        /*     if (ctx?.req) { */
-        /*       // To use SSR properly, you need to forward the client's headers to the server */
-        /*       // This is so you can pass through things like cookies when we're server-side rendering */
-        /**/
-        /*       // If you're using Node 18, omit the "connection" header */
-        /*       const { */
-        /*         // eslint-disable-next-line @typescript-eslint/no-unused-vars */
-        /* connection: _connection, */
-        /*         ...headers */
-        /*       } = ctx.req.headers; */
-        /*       return { */
-        /*         ...headers, */
-        /*         // Optional: inform server that it's an SSR request */
-        /*         'x-ssr': '1', */
-        /*         'Content-Type': 'application/json', */
-        /*       }; */
-        /*     } */
-        /*     return {}; */
-        /*   }, */
+        /* loggerLink({ */
+        /*   enabled: (opts) => */
+        /*     process.env.NODE_ENV === "development" || */
+        /*     (opts.direction === "down" && opts.result instanceof Error), */
         /* }), */
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+
+              // If you're using Node 18, omit the "connection" header
+              const {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                /* connection: _connection, */
+                ...headers
+              } = ctx.req.headers;
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                'x-ssr': '1',
+                'Content-Type': 'application/json',
+              };
+            }
+            return {};
+          },
+        }),
       ],
     };
   },
